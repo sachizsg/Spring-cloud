a66c7dc0253deea9b1d8b64f6629d23ef9d9846c
sachizsg <wrsachithra@gmail.com>
2020-06-24 13:06:59 +0530

    Create zuul service

---
+++ employee-zuul-service/src/main/java/com/example/javainuse/employeezuulservice/EmployeeZuulServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.javainuse.employeezuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeZuulServiceApplication.class, args);
+	}
+}
